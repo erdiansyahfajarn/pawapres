ee4273daa792a0e21747f775e7071cb097720d0e
erdiansyahfajarn <erdiansyahfajarn@gmail.com>
2026-01-02 10:59:54 +0000

    feat: enable serviceWorker

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 const CACHE = "portal-warga-v1";
 const ASSETS = [
+  "assets/web-app-manifest-192x192.png",
+  "assets/web-app-manifest-512x512.png",
+  "assets/favicon.ico",
+  "assets/favicon.svg",
+  "assets/favicon-96x96.png",
+  "assets/apple-touch-icon.png",
+
   "index.html",
   "style.css",
   "cms.js",
